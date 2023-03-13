439bdf2e2372888f36c569fc550f5b927eee8b05
Sophie Strausberg <sophie.strausberg@gmail.com>
2023-03-12 20:40:46 -0400

    Initial Commit

---
+++ RhythmApp/RhythmAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RhythmAppApp.swift
+//  RhythmApp
+//
+//  Created by Sophie Strausberg on 3/12/23.
+//
+
+import SwiftUI
+
+@main
+struct RhythmAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
